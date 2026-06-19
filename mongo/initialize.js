3bc591916dc1a2e7c6c1600903e9c225bdc568de
Scott Ballantyne <scottballantyne@protonmail.com>
2026-06-19 09:51:46 -0500

    trying to add different connection possibilities for mongo so it is easier to make lower level, more specific changes.

--- mongo/initialize.js
+++ mongo/initialize.js
@@ -1,11 +1,24 @@
-const mongoose = require('mongoose');
+const mongo = require('mongo');
 const path = require('path');
+
+
+// The mongoose initalizer returns a connection.  This one just 
+// returns a new client that can be connected and disconnected 
+// and not held open.  I would prefer to have then behave the 
+// same way and just return but I'm not sure how to handle the 
+// async.  Maybe I could just make both async?  Not ideal.
+
 
 function initialize(url) {
   if (url == undefined && process.env.MONGO_URL != undefined) {
     url = process.env.MONGO_URL;
   }
-  return mongoose.connect(url)
+  const client = new MongoClient(url);
+
+  return client
 }
 
 module.exports = initialize;
+
+
+
